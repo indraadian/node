5a370f6ea20a7ddde64b5b3a9928527897c0b3e1
Orthodock <indraadian15@gamil.com>
2019-09-12 23:35:01 +0700

    Orthodock

--- app.js
+++ app.js
@@ -1,34 +1,15 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
-<<<<<<< HEAD
-const bodyParser = require('body-parser');
-=======
 const mongoose = require('mongoose');
->>>>>>> 69ad09c2effa18c0ee35009e56678d452dbf67d5
 
 const productsRouts = require('./api/routes/products'); 
 const ordersRouts = require('./api/routes/orders'); 
 
-<<<<<<< HEAD
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use((req, res, next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONs') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).json({});
-    }
-    next();
-=======
 mongoose.connect('mongodb+srv://indraa:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-dtcc7.mongodb.net/test?retryWrites=true&w=majority', {
     // useMongoClient: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
->>>>>>> 69ad09c2effa18c0ee35009e56678d452dbf67d5
 });
 
 app.use(morgan('dev'));
--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT ||8000;
 
 server.listen(port);
 
-<<<<<<< HEAD
-
-// mongodb+srv://indraa:<password>@node-rest-shop-dtcc7.mongodb.net/test?retryWrites=true&w=majority
-//hsdf;jahdflsgdlasudguy
-=======
->>>>>>> 69ad09c2effa18c0ee35009e56678d452dbf67d5
