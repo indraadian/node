bd270e852ad4b3ae3c3714219ee5ee50aba4361e
Orthodock <indraadian15@gamil.com>
2019-09-12 22:59:27 +0700

    Orthodock

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,6 +8,10 @@ router.get('/', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const order = {
+        productId: req.body.productId,
+        quatity: req.body.quatity
+    }
     res.status(201).json({
         message: 'order was created'
     });
--- server.js
+++ server.js
@@ -7,3 +7,7 @@ const server = http.createServer(app);
 const port = process.env.PORT ||3000;
 
 server.listen(port);
+
+
+// mongodb+srv://indraa:<password>@node-rest-shop-dtcc7.mongodb.net/test?retryWrites=true&w=majority
+//hsdf;jahdflsgdlasudguy
