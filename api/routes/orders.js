9eacb5b49c67d7a6cd0137a63cb686d07e3c44b3
Orthodock <indraadian15@gamil.com>
2019-09-18 23:55:54 +0700

    p

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ router.get('/', (req, res, next) =>{
     Order
         .find()
         .select('product quantity _id')
+        .populate('product', 'name')
         .exec()
         .then(docs =>{
             res.status(200).json({
@@ -33,8 +34,8 @@ router.get('/', (req, res, next) =>{
         });
 });
 
-router.post('/', (req, res, next) =>{
-    Product.findById(req.body.productId)
+router.post('/', (req, res, next) =>{ 
+    Product.findById(req.body.productsId)
     .then(product => { 
         if (!product) {
             return res.status(404).json({
@@ -72,17 +73,52 @@ router.post('/', (req, res, next) =>{
 });
 
 router.get('/:orderId', (req, res ,next)=>{
-    res.status(200).json({
-        message: 'oerder details',
-        orderId: req.params.orderId
-    });
+
+    Order.findById(req.params.orderId)
+        .populate('product')
+        .exec()
+        .then(order => {
+            if(!order){
+                return res.status(404).json({
+                    message: 'Order not found'
+                });
+            }
+            res.status(200).json({
+                Order: order,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders'
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.delete('/:orderId', (req, res ,next)=>{
-    res.status(200).json({
-        message: 'oerder deleting',
-        orderId: req.params.orderId
-    });
+    Order.remove({_id: req.params.orderId})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Order Deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: {
+                        productId : 'ID', 
+                        quantity : 'Number'
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
