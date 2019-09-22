2e4c6d750b1be0aca4b32b6d4b9e7a61458ecf87
Orthodock <indraadian15@gamil.com>
2019-09-22 14:19:00 +0700

    check Auth

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
@@ -72,7 +73,7 @@ router.get('/', (req, res, next)=>{
 });
 
 //post atau mengentry atau membuat semua data 
-router.post('/', upload.single('productImage'), (req, res, next)=>{
+router.post('/', upload.single('productImage'), checkAuth, (req, res, next)=>{
     const product = new Product({
          _id: new mongoose.Types.ObjectId(),
          name: req.body.name,
